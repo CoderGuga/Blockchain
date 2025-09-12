760e50e3fbbde009442a31ab8ddcc9321dfa3b95
Gabrielius Rutkauskas <gabrielius.rut@gmail.com>
2025-09-12 09:56:01 +0200

    hashs first

---
+++ main/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Numerics;
+
+class Program
+{
+
+
+    static void Main()
+    {
+        Console.WriteLine("Name: ");
+
+        string filePath = "Tekstas.txt";
+
+        //string filePath = Console.ReadLine();
+
+        string text = File.ReadAllText(filePath);
+        Console.WriteLine("File contents:");
+        Console.WriteLine(text);
+
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+        string bits = string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+        Console.WriteLine(bits); // Output: 01001000 01100101 01101100 01101100 01101111
+
+        BigInteger number = new BigInteger(bytes);
+        Console.WriteLine("Number representation:");
+        Console.WriteLine(number);
+    }
+}
