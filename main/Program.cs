8349ff8e4b17f672cfdc8992173636321f411fb8
Gabrielius Rutkauskas <gabrielius.rut@gmail.com>
2025-09-16 20:01:03 +0200

    fixed to have avanlance

--- main/Program.cs
+++ main/Program.cs
@@ -16,36 +16,21 @@ class Program
         //string filePath = Console.ReadLine();
 
         string text = File.ReadAllText(filePath);
-        Console.WriteLine("Input:");
-        Console.WriteLine(text);
+        //Console.WriteLine("Input:\n" + text);
 
-        byte[] bytes = Encoding.UTF8.GetBytes(text);
+        byte[] hash = GetHashString(text, 32);
+        BigInteger bigInteger = new BigInteger(hash);
+        BigInteger mult = MultNumbers(bigInteger);
+        hash = GetHashString(mult.ToString(), 32);
+        //int intValue = BitConverter.ToInt32(firstHash, 0);
+        //byte[] newHash = ShuffleBytes(firstHash, intValue);
+        //string hexString = BitConverter.ToString(newHash).Replace("-", "");
 
-        //string bits = string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
-        //Console.WriteLine(bits); // Output: 01001000 01100101 01101100 01101100 01101111
+        string hexString = BitConverter.ToString(hash).Replace("-", "");
 
-        while (bytes.Length < 64)
-        {
-            BigInteger number = new BigInteger(bytes);
-            BigInteger shiftedLeft = number << 1;
-            byte[] shiftedLeftBytes = shiftedLeft.ToByteArray();
-
-            bytes = bytes.Concat(shiftedLeftBytes).ToArray();
-        }
-
-        byte[] firstBytes = bytes.Take(32).ToArray();
-        bytes = bytes.Skip(32).ToArray();
-
-        while (bytes.Length >= 32)
-        {
-            byte[] xorResult = XorArrays(firstBytes, bytes.Take(32).ToArray());
-            bytes = bytes.Skip(32).ToArray();
-            firstBytes = xorResult;
-        }
-
-        string hexString = BitConverter.ToString(firstBytes).Replace("-", "");
         Console.WriteLine("Hash:");
         Console.WriteLine(hexString + "\n");
+        //Console.WriteLine("int value:" + intValue);
     }
 
 
@@ -58,5 +43,84 @@ class Program
             result[i] = (byte)(a[i] ^ b[i]);
         }
         return result;
+    }
+
+    static byte[] ShiftBytes(byte[] bytes, int shiftN)
+    {
+        BigInteger number = new BigInteger(bytes);
+        BigInteger shiftedLeft = number << shiftN;
+        byte[] shiftedLeftBytes = shiftedLeft.ToByteArray();
+
+        // Prepend shiftedLeftBytes to bytes
+        byte[] result = shiftedLeftBytes.Concat(bytes).ToArray();
+        return result;
+    }
+
+    static void WriteBits(byte[] bytes)
+    {
+        string bits = string.Join(" ", bytes.Select(b => Convert.ToString(b, 2).PadLeft(8, '0')));
+        Console.WriteLine(bits);
+    }
+
+    static byte[] GetHashString(string text, int lenght)
+    {
+        byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+        //WriteBits(bytes.Take(1).ToArray());
+        //WriteBits(ShiftBytes(bytes.Take(1).ToArray(), 1));
+
+        while (bytes.Length < lenght * 2)
+        {
+            byte[] shiftedLeftBytes = ShiftBytes(bytes, 1);
+
+            bytes = bytes.Concat(shiftedLeftBytes).ToArray();
+        }
+
+        byte[] firstBytes = bytes.Take(lenght).ToArray();
+        bytes = bytes.Skip(lenght).ToArray();
+
+        while (bytes.Length > 0)
+        {
+            if (bytes.Length < lenght)
+            {
+                byte[] padded = new byte[lenght];
+                Array.Copy(bytes, padded, bytes.Length);
+                bytes = padded;
+            }
+            //firstBytes = ShiftBytes(firstBytes, 1);
+            byte[] xorResult = XorArrays(firstBytes, bytes.Take(lenght).ToArray());
+            bytes = bytes.Skip(lenght).ToArray();
+            firstBytes = xorResult;
+        }
+
+        return firstBytes;
+
+    }
+
+    static BigInteger MultNumbers(BigInteger bigInteger)
+    {
+        BigInteger res = 1;
+        byte[] bytes = bigInteger.ToByteArray();
+        foreach (byte num in bytes)
+        {
+            res *= num;
+        }
+        return res;
+    }
+
+    static byte[] ShuffleBytes(byte[] array, int seed)
+    {
+        Random rng = new Random(seed);
+        int n = array.Length;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            byte temp = array[k];
+            array[k] = array[n];
+            array[n] = temp;
+        }
+
+        return array;
     }
 }
--- main/obj/Debug/net9.0/main.AssemblyInfo.cs
+++ main/obj/Debug/net9.0/main.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+760e50e3fbbde009442a31ab8ddcc9321dfa3b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd83fd99462ed51b4c58e896e32e1858fff5ea1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
